43c02ce41c6e621ac4bdf2346d7f3ae8f6085d0e
Ayushkumar242004 <143803366+Ayushkumar242004@users.noreply.github.com>
2024-07-09 16:32:30 +0530

    Second commit

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://91e9e25e6e6c8eb57fc887cad8a14d5c@o4507570775392256.ingest.us.sentry.io/4507570778669056",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
